d045764ce48503565518669d6272dbf3d2c1b7ef
Andres Peña <elsenorcito@gmail.com>
2022-06-15 21:32:02 -0500

    Base Project with Dependencies
    Signed-off-by: Andres Peña <elsenorcito@gmail.com>

---
+++ src/test/java/com/parameta/pruebaParameta/PruebaParametaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parameta.pruebaParameta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaParametaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
